937459eccc2d317dcb0712ffa4fc950b253580d1
Old_usr <120130487+Teamolduser@users.noreply.github.com>
2025-05-27 10:25:13 +0530

    Update decode-wa-message.ts

--- src/Utils/decode-wa-message.ts
+++ src/Utils/decode-wa-message.ts
@@ -3,7 +3,7 @@ import { proto } from '../../WAProto'
 import { SignalRepository, WAMessageKey } from '../Types'
 import { areJidsSameUser, BinaryNode, getBinaryNodeChild, isJidBroadcast, isJidGroup, isJidMetaIa, isJidNewsletter, isJidStatusBroadcast, isJidUser, isLidUser } from '../WABinary'
 import { BufferJSON, unpadRandomMax16 } from './generics'
-import { ILogger } from './logger
+import { ILogger } from './logger'
 
 export const NO_MESSAGE_FOUND_ERROR_TEXT = 'Message absent from node'
 export const MISSING_KEYS_ERROR_TEXT = 'Key used already or never filled'
