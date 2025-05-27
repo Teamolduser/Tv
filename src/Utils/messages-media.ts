4af64a2577ec40777d7c81d386ac9e9b51556dea
Its-me-001 <khushijain2901@mail.com>
2025-05-27 12:08:37 +0530

    #newsletr

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -179,6 +179,76 @@ export const generateProfilePicture = async(mediaUpload: WAMediaUpload) => {
 	}
 }
 
+export const generateProfilePictureFull = async(img) => {
+	const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+	const jimp = await read(img)
+	const min = Math.min(jimp.getWidth(), jimp.getHeight())
+	const cropped = jimp.crop(0, 0, jimp.getWidth(), jimp.getHeight())
+	let width = jimp.getWidth(),
+		hight = jimp.getHeight(),
+		ratio;
+	if (width > hight) {
+		ratio = jimp.getWidth() / 720
+	} else {
+		ratio = jimp.getWidth() / 324
+	};
+	width = width / ratio;
+	hight = hight / ratio;
+	img = cropped.quality(100).resize(width, hight).getBufferAsync(MIME_JPEG);
+	return {
+		img: await cropped.quality(100).resize(width, hight).getBufferAsync(MIME_JPEG),
+	}
+}
+
+export const generateProfilePictureFP = async(buffer) => {
+	const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+    const jimp = await Jimp.read(buffer);
+    const min = jimp.getWidth();
+    const max = jimp.getHeight();
+    const cropped = jimp.crop(0, 0, min, max);
+    return {
+      img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+      preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+    };
+}
+
+export const generatePP = async(buffer) => {
+const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+    const jimp = await Jimp.read(buffer);
+    const min = jimp.getWidth();
+    const max = jimp.getHeight();
+    const cropped = jimp.crop(0, 0, min, max);
+    return {
+      img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+      preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+    };
+  }
+  
+  export const changeprofileFull = async(img) => {
+    const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+	const jimp = await read(img)
+	const min = Math.min(jimp.getWidth(), jimp.getHeight())
+	const cropped = jimp.crop(0, 0, jimp.getWidth(), jimp.getHeight())
+	let width = jimp.getWidth(),
+		hight = jimp.getHeight(),
+		ratio;
+	if (width > hight) {
+		ratio = jimp.getWidth() / 720
+	} else {
+		ratio = jimp.getWidth() / 324
+	};
+	width = width / ratio;
+	hight = hight / ratio;
+	img = cropped.quality(100).resize(width, hight).getBufferAsync(MIME_JPEG);
+	return {
+		img: await cropped.quality(100).resize(width, hight).getBufferAsync(MIME_JPEG),
+	}
+}
+
 /** gets the SHA256 of the given media message */
 export const mediaMessageSHA256B64 = (message: WAMessageContent) => {
 	const media = Object.values(message)[0] as WAGenericMediaMessage
@@ -606,7 +676,7 @@ export const getWAUploadToServer = (
 	{ customUploadHosts, fetchAgent, logger, options }: SocketConfig,
 	refreshMediaConn: (force: boolean) => Promise<MediaConnInfo>,
 ): WAMediaUploadFunction => {
-	return async(filePath, { mediaType, fileEncSha256B64, timeoutMs }) => {
+	return async(filePath, { mediaType, fileEncSha256B64, newsletter, timeoutMs }) => {
 		// send a query JSON to obtain the url & auth token to upload our media
 		let uploadInfo = await refreshMediaConn(false)
 
@@ -614,6 +684,11 @@ export const getWAUploadToServer = (
 		const hosts = [ ...customUploadHosts, ...uploadInfo.hosts ]
 
 		fileEncSha256B64 = encodeBase64EncodedStringForUpload(fileEncSha256B64)
+		
+		let media = MEDIA_PATH_MAP[mediaType]
+		if (newsletter) {
+			media = media?.replace('/mms/', '/newsletter/newsletter-')
+		}
 
 		for(const { hostname } of hosts) {
 			logger.debug(`uploading to "${hostname}"`)
